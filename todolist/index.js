cb8a0c9797bd570a5f65afa25b78cf31eef01ed8
dhvanichhajed <clanguage3289@gmail.com>
2024-11-30 14:52:22 +0530

    todolist

--- todolist/index.js
+++ todolist/index.js
@@ -1,21 +1,61 @@
 const express = require('express');
-const port = 8485;
 const server = express();
-const path = require('path');
+const port = 3000;
 
 server.set('view engine', 'ejs');
 server.use(express.urlencoded());
 
-const tasks = [{ task: "jhed" }];
-const completedTasks = [{ task: "dfj nbfhj  " }];
+let todoList = [
+    { task: "Sample task 1" },
+    { task: "Sample task 2" }
+];
 
-server.get('/', (request, response) => {
-    response.render('index')
+let finishedtask = [
+    { task: "completed task 1" },
+    { task: "completed task 2" }
+]
+
+server.get('/', (req, res) => {
+    return res.render('index', { todoList, finishedtask });
 });
 
-server.post("/", (request, response) => {
+// -------------------------- ADD TASK -----------------------------
+server.post("/addTask", (req, res) => {
+    let { task } = req.body;
+    let newTask = { task }
+    if (task) {
+        todoList.push(newTask);
+        console.log('Task added:');
+    } else {
+        console.log('Task cannot be empty');
+    }
+    return res.redirect("/");
+});
 
-})
+// -------------------------- DELETE TASK -----------------------------
+server.get("/deleteTask/:id", (req, res) => {
+    let id = req.params.id
+    todoList = todoList.filter((val, index) => {
+        return index != id
+    })
+    return res.redirect("/");
+});
+
+// -------------------------- COMPLETED TASK -----------------------------
+server.get("/completedtask/:id", (req, res) => {
+    let id = req.params.id;
+    let checkedOffTask = todoList.splice(id, 1)[0];
+    finishedtask.push(checkedOffTask);
+    res.redirect("/");
+});
+
+// -------------------------- INCOMPLETE TASK -----------------------------
+server.get("/incompletedtask/:id", (req, res) => {
+    let id = req.params.id;
+    let uncheckedTask = finishedtask.splice(id, 1)[0];
+    todoList.push(uncheckedTask);
+    res.redirect("/");
+});
 
 server.listen(port, (err) => {
     if (err) {
@@ -24,3 +64,5 @@ server.listen(port, (err) => {
         console.log(`Server start at http://localhost:${port}`);
     }
 });
+
+
